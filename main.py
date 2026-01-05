cdb2acf497c280ba272dadf9a53dbf36ecef05f3
e2500465 <adilelb2007@gmail.com>
2026-01-05 14:53:42 +0100

    programme 1

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+import csv
+
+etudiants = [
+    {"ID": 1, "Nom": "Schmitt", "Prenom": "Albert", "Note": 9},
+    {"ID": 2, "Nom": "Al-Hakim", "Prenom": "Yasmine", "Note": 17},
+    {"ID": 3, "Nom": "Tran", "Prenom": "Sebastien", "Note": 12},
+    {"ID": 4, "Nom": "Meyer", "Prenom": "Claire", "Note": 16},
+    {"ID": 5, "Nom": "Kobayashi", "Prenom": "Kaito", "Note": 11}
+]
+
+with open('fichier.csv', mode='w', encoding='utf-8', newline='') as file:
+    champs = ["ID", "Nom", "Prenom", "Note"]
+    fichier = csv.DictWriter(file, fieldnames=champs)
+    fichier.writeheader()
+    fichier.writerows(etudiants)
 
 
-print('hello')
+with open('resultats.csv', mode='w', encoding='utf-8', newline='') as file:
+    champs = ["Nom", "Prénom", "Résultat"]
+    fichier = csv.DictWriter(file, fieldnames=champs)
+    fichier.writeheader()
+
+    for i in etudiants:
+        resultat = "Admis" if i["Note"] >= 10 else "Refusé"
+        fichier.writerow({"Nom": i["Nom"],"Prénom": i["Prenom"],"Résultat": resultat})
